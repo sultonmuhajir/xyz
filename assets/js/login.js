704da6cc6b238038c4e516f2d0a7c88991657b64
Sulton Muhajir <sulton.muhajir28@gmail.com>
2021-07-07 12:51:55 +0700

    ~ form

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,29 +5,39 @@ window.addEventListener("load", function() {
    }, 500)
 });
 
-const x = document.getElementById("pw");
-const e = document.querySelector(".error")
 
+const inp = document.getElementById("pw");
+const form = document.querySelector("form")
+const error = document.querySelector(".error")
+const btn = document.querySelector(".btn")
 
-x.addEventListener("keyup", function () {
-   this.style.border = "1px solid #aaa";
-   e.style.visibility = "hidden";
-});
 
 function showPassword() {
-   if (x.type === "password") {
-      x.type = "text";
+   if (inp.type === "password") {
+      inp.type = "text";
    } else {
-      x.type = "password";
+      inp.type = "password";
    }
 }
 
+
+inp.addEventListener("keyup", function(event) {
+   this.style.border = "1px solid #aaa";
+   error.style.visibility = "hidden";
+   if (event.keyCode === 13) {
+      event.preventDefault();
+      btn.click();
+   }
+});
+
+
+form.onsubmit = () => { return false; }
 function login() {
-   if (x.value == "" || x.value != "sulton..") {
-      x.value = "";
-      x.focus();
-      x.style.border = "1px solid red";
-      e.style.visibility = "visible";
+   if (inp.value == "" || inp.value != "sulton..") {
+      inp.value = "";
+      inp.focus();
+      inp.style.border = "1px solid red";
+      error.style.visibility = "visible";
    } else {
       document.location = "home.html"
    }
